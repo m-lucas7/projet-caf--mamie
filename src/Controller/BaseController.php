dd78d7518dbf71d28d213b4b2d1d22b515ddf027
Lucas Moniez <lucas.moniez@epsi.fr>
2023-10-31 15:23:32 +0100

    Formulaire

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -4,7 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\AjoutCafeType;
 
 class BaseController extends AbstractController
 {
@@ -17,7 +18,7 @@ class BaseController extends AbstractController
     #[Route('/cafe', name: 'app_cafe')]
     public function cafe(): Response
     {
-        $form = $this->createForm(CafeType::class);
+        $form = $this->createForm(AjoutCafeType::class);
         return $this->render('base/cafe.html.twig', [
             'form' => $form->createView()
         ]);
--- src/Form/AjoutCafeType.php
+++ src/Form/AjoutCafeType.php
@@ -6,15 +6,27 @@ use App\Entity\TypeCafe;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AjoutCafeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('designation')
-            ->add('origine')
-        ;
+            ->add('nom', TextType::class, ['attr' => ['class' => 'form-control'], 'label_attr' => ['class' =>
+            'fw-bold']])
+            ->add('prix', TextType::class, ['attr' => ['class' => 'form-control'], 'label_attr' => ['class' =>
+            'fw-bold']])
+            ->add('origine', TextType::class, ['attr' => ['class' => 'form-control'], 'label_attr' => ['class' =>
+            'fw-bold']])
+            ->add('description', TextareaType::class, ['attr' => ['class' => 'form-control', 'rows' => '7', 'cols'
+            => '7'], 'label_attr' => ['class' => 'fw-bold']])
+            ->add('envoyer', SubmitType::class, [
+                'attr' => ['class' => 'btn bg-primary text-white m-4'],
+                'row_attr' => ['class' => 'text-center'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
